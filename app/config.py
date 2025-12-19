9da536f27ce35fdc8beaa3dd9864b95d5f471e0a
divyani <ikrakizoi2607@gmail.com>
2025-12-19 23:25:57 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,9 @@
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/products"
+)
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
