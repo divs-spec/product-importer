aa91179ff7cc2a26ea3f8b9674c8cf21339b930f
divyani <ikrakizoi2607@gmail.com>
2025-12-20 00:02:23 +0530

    Update products.py

--- app/api/products.py
+++ app/api/products.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.orm import Session
-from ..db import get_db
 from ..database import SessionLocal
 from ..models import Product
 from ..schemas import ProductOut, ProductCreate
