2c730baf0b710ecd71fb4c7688bde484edcb23d5
divyani <ikrakizoi2607@gmail.com>
2025-12-19 23:19:11 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from .database import Base, engine
+from .api import products, upload, jobs, webhooks
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(products.router)
+app.include_router(upload.router)
+app.include_router(jobs.router)
+app.include_router(webhooks.router)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/")
+def home():
+    return templates.TemplateResponse("index.html", {"request": {}})
